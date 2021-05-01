31138b6c75288f489cf9be9669834934a9e8868b
Gopi Kumar Kaushik <gopikumarkaushik9065@gmail.com>
2021-05-01 20:36:21 +0530

    data base connected

--- configs/db.js
+++ configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 function dbConnect() {
 
     try {
-        mongoose.connect('mongodb+srv://gopi:xZR7OYREtQ8ZPxeU@cluster0.s5h8o.mongodb.net/gopi_db?retryWrites=true&w=majority', { useNewUrlParser: true },{ useUnifiedTopology: true })
+        mongoose.connect('mongodb+srv://gopi:xZR7OYREtQ8ZPxeU@cluster0.s5h8o.mongodb.net/student_friend_db?retryWrites=true&w=majority', { useNewUrlParser: true },{ useUnifiedTopology: true })
         console.log("Database connected");
     } catch (err) {
         console.log("Database not connected :",err);
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
+const connectDB = require("./configs/db")
+
+
+//db connection
+connectDB();
+
 
 app.get('/', (req,res) => {
     res.send("Hello world")

ff26cc216d02e2d60405d75e5b64b78c8a40ec71
Gopi Kumar Kaushik <gopikumarkaushik9065@gmail.com>
2021-05-02 00:54:23 +0530

    data postion added

--- server.js
+++ server.js
@@ -3,22 +3,50 @@ const app = express();
 const port = 3001;
 const connectDB = require("./configs/db")
 const student = require('./models/student')
+const bodyParser = require("body-parser");
+
 
 //db connection
 connectDB();
 
+//body parse configuration
+app.use(bodyParser.urlencoded({extended : true}));
+
+app.use(bodyParser.json());
+
+//routes
 app.get('/:userName/:password', async (req,res) => {
     const user_name = req.params.userName;
     const password = req.params.password;
     const result = await student.findOne({userName : user_name, password : password }).exec();
+    if(result.length === 0){
+        res.send("No user found");
+    }else{
+        res.send(result)
+    }
+})
+
+
+app.post("/", async (req, res) => {
+   let user_name = req.body.userName;
+   let password = req.body.password;
+   let todo = req.body.todo;
+   let alarm = req.body.alarm;
+   const result = await student.findOne({userName : user_name, password : password }).exec();
+    if(result === null){
+        res.send("no user found");
+    }else{
+        await student.updateOne({userName : user_name, password : password}, {todo : todo, alarm : alarm}).exec();
+        res.setHeader('Access-Control-Allow-Origin' , '*')
+        res.send("Student updated"); 
+    }
     res.send(result);
+  
+   
+
 })
 
-
-app.post("/", (req, res) => {
-    
-})
-
+//listening
 app.listen(port ,()=>{
     console.log(`Example app listening at http://localhost:${port}`);
 }) 

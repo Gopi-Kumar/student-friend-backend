8877ea04c705044d45a8bec73c661570500a5176
Gopi Kumar Kaushik <gopikumarkaushik9065@gmail.com>
2021-07-14 14:04:29 +0530

    some fixes

--- server.js
+++ server.js
@@ -87,6 +87,6 @@ app.post("/newuser/:username/:password", async (req,res)=>{
 })
 
 //listening
-app.listen(port ,()=>{
-    console.log(`Example app listening at http://localhost:${port}`);
-}) 
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
